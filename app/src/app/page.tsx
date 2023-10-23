e548eb0130a0f5d0a514ba9bfb641625f6b9c4fc
Ismail Halili <i.halili99@hotmail.com>
2023-10-23 12:20:05 +0200

    example: add example cms api and request in app

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from 'next/image'
 
+// Example request to cms
+const cmsContentType = 'announcements'
+fetch(`${process.env.CMS_API}/${cmsContentType}`, {
+  headers: {
+    authorization: `Bearer ${process.env.CMS_TOKEN}`,
+  },
+})
+  .then((ok) => ok.json())
+  .then((res) => console.log(res))
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
---
+++ cms/src/api/announcement/routes/announcement.ts
@@ -0,0 +1,7 @@
+/**
+ * announcement router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::announcement.announcement');
