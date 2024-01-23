ead6817dadcb81a183ee51ebb28a29851bf9ac80
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-01-23 08:25:33 +0100

    feat(FE): add styling to card, remove unused import

--- web/src/components/Card/Card.tsx
+++ web/src/components/Card/Card.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 type CardProps = {
     children: React.ReactNode;
 };
 
 export default function Card({ children }: CardProps) {
-    return <div>{children}</div>;
+    return <div className="rounded-xl pt-6 px-6 pb-10 bg-gray-1">{children}</div>;
 }
