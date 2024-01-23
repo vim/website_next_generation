232ca72a60d0ebf95866c33536bfb56ea581313e
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-01-23 08:24:53 +0100

    feat(FE): add custom gray color

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,28 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
-        tertiary: 'var(--color-tertiary)',
-        'primary-text': 'var(--color-primary-text)',
-        'secondary-text': 'var(--color-secondary-text)',
-        'accent-text': 'var(--color-primary)',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                primary: 'var(--color-primary)',
+                secondary: 'var(--color-secondary)',
+                tertiary: 'var(--color-tertiary)',
+                'primary-text': 'var(--color-primary-text)',
+                'secondary-text': 'var(--color-secondary-text)',
+                'accent-text': 'var(--color-primary)',
+                'gray-1': 'var(--gray-1)',
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
