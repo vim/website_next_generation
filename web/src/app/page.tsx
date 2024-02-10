90fd6c336236503022b3c0fbc5eacd500e6af7ba
hanEck <hannes.eckelt@t-online.de>
2024-02-10 16:55:45 +0100

    feat(web) => Add about vim section on home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,6 +6,7 @@ import ContentSection from "@/components/Sections/ContentSection";
         __component:string
         title?:string,
         description?:string,
+        buttonTitle?:string
     }
 
     type HomeComponents = {
@@ -41,26 +42,32 @@ import ContentSection from "@/components/Sections/ContentSection";
 
 export default async function Home() {
     const singleTypeComponentHomeData = await getSingleTypeComponentHomeData()
-
+    console.log(singleTypeComponentHomeData)
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             {Object.keys(singleTypeComponentHomeData).map((key) => {
-                if(key ==='welcome-section'){
-                    const component = singleTypeComponentHomeData[key]
-                    return (
-                        <div key={key} className="flex flex-col items-center justify-center h-screen">
-                    <div  className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-                        <div className="flex flex-col justify-end">
-                            <ContentSection title={component.title} description={component.description} />
-                        </div>
-                        <div className="flex justify-end items-end">
-                            <ButtonPrimary>Download</ButtonPrimary>
-                        </div>
-                        </div>
-                    </div>)
+                const component = singleTypeComponentHomeData[key]
+                switch(key){
+                    case 'welcome-section':
+                        return (
+                            <div key={key} className="flex flex-col items-center justify-center h-screen">
+                                <div  className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+                                    <div className="flex flex-col justify-end">
+                                        <ContentSection title={component.title} description={component.description}/>
+                                    </div>
+                                    <div className="flex justify-end items-end">
+                                        <ButtonPrimary>{component.buttonTitle}</ButtonPrimary>
+                                    </div>
+                                </div>
+                            </div>)
+                    case 'about-vim-section':
+                        return(
+                            <div key = {key}>
+                                <ContentSection title={component.title} description={component.description}/>
+                            </div>
+                        )
                 }
             })}
-            <h2>This is another element</h2>
         </main>
     );
 }
