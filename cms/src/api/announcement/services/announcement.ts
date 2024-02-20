9b15873f9e5be97622c0cce676b87df7cb47c245
Ismail Halili <i.halili99@hotmail.com>
2024-02-20 16:43:51 +0100

    feat(cms): Add news content type 'newsposts'  to cms

--- cms/src/api/announcement/services/announcement.ts
+++
@@ -1,7 +0,0 @@
-/**
- * announcement service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::announcement.announcement');
---
+++ cms/src/api/newspost/controllers/newspost.ts
@@ -0,0 +1,7 @@
+/**
+ * newspost controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::newspost.newspost');
