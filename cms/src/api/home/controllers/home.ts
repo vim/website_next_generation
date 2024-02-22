a35a27636010f0af8739ab471e27c60e91f9520f
Ismail Halili <i.halili99@hotmail.com>
2024-02-22 18:58:55 +0100

    feat(cms): add news to home-page response, customized home-page controller

--- cms/src/api/home/controllers/home.ts
+++ cms/src/api/home/controllers/home.ts
@@ -2,6 +2,18 @@
  * home controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::home.home');
+export default factories.createCoreController(
+  "api::home.home",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const response = await super.find(ctx);
+      const news = await strapi.entityService.findMany(
+        "api::newspost.newspost"
+      );
+
+      return { ...response, news };
+    },
+  })
+);
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,82 +1,77 @@
 import qs from 'qs';
-import HeroSection from "@/components/Sections/HeroSection";
-import ContentSection from "@/components/Sections/ContentSection";
-import ButtonPrimary from "@/components/Buttons/ButtonPrimary";
-
-
+import HeroSection from '@/components/Sections/HeroSection';
+import ContentSection from '@/components/Sections/ContentSection';
+import ButtonPrimary from '@/components/Buttons/ButtonPrimary';
 
 const query = qs.stringify(
     {
-        populate: ['body, body.listItems, body.cta','body.button'],
+        populate: ['body, body.listItems, body.cta', 'body.button'],
     },
     {
-      encodeValuesOnly: true,
+        encodeValuesOnly: true,
+    },
+);
+
+type Section = {
+    id: number;
+    __component: string;
+    headline?: string;
+    listItems?: { item: string; id: number }[];
+    text: string;
+    cta: { id: string; text: string; type: string; url: string };
+};
+
+type HomeComponents = {
+    [key: string]: Omit<Section, 'id' | '__component'>;
+};
+
+async function fetchSingleTypeComponentHome() {
+    const response = await fetch(`${process.env.CMS_API}/home?${query}`);
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error('Fetching single type component home failed');
     }
-  );
+    return data;
+}
 
-    type Section = {
-        id:number,
-        __component:string,
-        headline?:string,
-        listItems?: {item:string,id:number}[];
-        text:string,
-        cta: {id:string, text:string,type:string,url:string},
-    }
+function unwrapSingleTypeComponentHome(body: Section[]): HomeComponents {
+    const unwrapedComponents: HomeComponents = {};
+    body.forEach((section) => {
+        const sectionType = section.__component.split('.')[1];
+        const { id, __component, ...rest } = section;
+        unwrapedComponents[sectionType + '.' + id] = rest;
+    });
+    return unwrapedComponents;
+}
 
-    type HomeComponents = {
-        [key: string]: Omit<Section, 'id' | '__component'>;
-      };
-
-    async function fetchSingleTypeComponentHome(){
-        const response = await fetch(`${process.env.CMS_API}/home?${query}`)
-        if(!response.ok){
-            throw new Error('Fetching single type component home failed');
-        }
-        return response.json();
-    }
-
-    function unwrapSingleTypeComponentHome(body: Section[]): HomeComponents {
-        const unwrapedComponents: HomeComponents = {};
-        body.forEach(section => {
-            const sectionType = section.__component.split('.')[1];
-            const { id, __component, ...rest } = section;
-            unwrapedComponents[sectionType+"."+id] = rest;
-        });
-        return unwrapedComponents;
-      }
-
-    async function  getSingleTypeComponentHomeData(){
-        const response = await fetchSingleTypeComponentHome()
-        return unwrapSingleTypeComponentHome(response.data.attributes.body) 
-    }
+async function getSingleTypeComponentHomeData() {
+    const response = await fetchSingleTypeComponentHome();
+    return unwrapSingleTypeComponentHome(response.data.attributes.body);
+}
 
 export default async function Home() {
-    const singleTypeComponents = await getSingleTypeComponentHomeData()    
+    const singleTypeComponents = await getSingleTypeComponentHomeData();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            {
-                Object.keys(singleTypeComponents).map((key) => {
-                    const component = singleTypeComponents[key]
-                    if(key.split('.')[0]==='hero-section'){
-                        return (
-                            <div key={key} className="flex flex-col items-center justify-center h-screen">
-                                <div  className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-                                    <div className="flex flex-col justify-end">
-                                        <HeroSection headline={component.headline} listItems={component.listItems}/>
-                                    </div>
-                                    <div className="flex justify-end items-end">
-                                        <ButtonPrimary>{component.cta.text}</ButtonPrimary>
-                                    </div>
+            {Object.keys(singleTypeComponents).map((key) => {
+                const component = singleTypeComponents[key];
+                if (key.split('.')[0] === 'hero-section') {
+                    return (
+                        <div key={key} className="flex flex-col items-center justify-center h-screen">
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+                                <div className="flex flex-col justify-end">
+                                    <HeroSection headline={component.headline} listItems={component.listItems} />
                                 </div>
-                            </div>)
-                    }
-                    else{
-                        return(<div>
-                            {/* Add other elements of home page*/}
-                        </div>) 
-                    }
-                })
-            }
+                                <div className="flex justify-end items-end">
+                                    <ButtonPrimary>{component.cta.text}</ButtonPrimary>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                } else {
+                    return <div>{/* Add other elements of home page*/}</div>;
+                }
+            })}
         </main>
     );
 }
