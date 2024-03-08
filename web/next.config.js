1ab3f99c8eab346242cc9291a34245daeae14a68
Johannes <johannes.munker@gmail.com>
2024-03-08 19:26:49 +0100

    style(web): run prettier

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpackDevMiddleware: (config) => {
-        config.watchOptions = {
-            poll: 1000,
-            aggregateTimeout: 300,
-        };
-        return config;
-    },
+	webpackDevMiddleware: config => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300,
+		};
+		return config;
+	},
 };
 
 module.exports = nextConfig;
