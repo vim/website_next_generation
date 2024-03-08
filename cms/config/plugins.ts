50aa79d9709e7409a3c939d08bf2a12f4b02d3fa
Johannes <johannes.munker@gmail.com>
2024-03-08 17:28:12 +0100

    feat(plugins): set importOnBootstrap flag in config-sync plugin to true in non dev environments

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,37 +1,37 @@
 // ./config/plugins.js`
 "use strict";
 
-module.exports = {
-  menus: {
-    config: {
-      layouts: {
-        menuItem: {
-          link: [
-            {
-              input: {
-                label: "Page Relation",
-                name: "page_relation",
-                type: "relation",
-              },
-              grid: {
-                col: 6,
-              },
+module.exports = ({ env }) => ({
+    menus: {
+        config: {
+            layouts: {
+                menuItem: {
+                    link: [
+                        {
+                            input: {
+                                label: "Page Relation",
+                                name: "page_relation",
+                                type: "relation",
+                            },
+                            grid: {
+                                col: 6,
+                            },
+                        },
+                    ],
+                },
             },
-          ],
         },
-      },
     },
-  },
-  "config-sync": {
-    enabled: true,
-    config: {
-      syncDir: "config/sync/",
-      minify: false,
-      soft: false,
-      importOnBootstrap: false,
-      customTypes: [],
-      excludedTypes: [],
-      excludedConfig: [],
+    "config-sync": {
+        enabled: true,
+        config: {
+            syncDir: "config/sync/",
+            minify: false,
+            soft: false,
+            importOnBootstrap: env("NODE_ENV") !== "development",
+            customTypes: [],
+            excludedTypes: [],
+            excludedConfig: [],
+        },
     },
-  },
-};
+});
