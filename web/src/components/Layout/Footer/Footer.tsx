04e71f5684275c4ad15171650e1a7e0e798d7e7a
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-21 13:47:31 +0100

    feat(web): add spacing to footer according to styleguide

--- web/src/components/Layout/Footer/Footer.tsx
+++ web/src/components/Layout/Footer/Footer.tsx
@@ -3,9 +3,13 @@ import React from "react";
 export default function Footer() {
 	return (
 		<footer className="w-full flex justify-center items-center pb-3">
-			<a href="/impressum">Impressum</a>
-			<span className="border-x-2 border-primary h-9 mx-3 w-0" />
-			<a href="/copyright">Copyright</a>
+			<a className="link" href="/impressum">
+				Impressum
+			</a>
+			<span className="border-x-2 border-primary h-9 mx-[48px] w-0" />
+			<a className="link" href="/copyright">
+				Copyright
+			</a>
 		</footer>
 	);
 }
