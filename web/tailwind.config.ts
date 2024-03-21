a827530f75729f0679663e48a79d0da42cd49e01
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-21 09:40:23 +0100

    feat(web): add colors according to styleguide

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -4,20 +4,13 @@ const config: Config = {
 	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
 			colors: {
 				primary: "var(--color-primary)",
-				secondary: "var(--color-secondary)",
-				tertiary: "var(--color-tertiary)",
-				"primary-text": "var(--color-primary-text)",
-				"secondary-text": "var(--color-secondary-text)",
-				"accent-text": "var(--color-primary)",
+				"secondary-text": "var(--c-secondary-text)",
 				"gray-1": "var(--c-gray-1)",
 				"gray-2": "var(--c-gray-2)",
 				"gray-3": "var(--c-gray-3)",
+				"gray-4": "var(--c-gray-4)",
 			},
 		},
 	},
