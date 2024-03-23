d741a55b115e4875a4e8181dcaf2aebae6d81e4b
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-23 11:38:11 +0100

    fix(web): order of imports

--- web/src/components/Layout/Navigation/Navigation.tsx
+++ web/src/components/Layout/Navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { IconUserCircle } from "@tabler/icons-react";
 import Image from "next/image";
 import Link from "next/link";
+import { IconUserCircle } from "@tabler/icons-react";
 
 type NavigationProps = {
 	menu: Page[];
