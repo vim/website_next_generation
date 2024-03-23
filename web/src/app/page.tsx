7fdb45a976b8c98504c6345bb3b0aa8fe02c0be0
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-23 19:38:58 +0100

    wip(web): add test code for check styling of headings

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
 	return (
 		<main>
 			<h1 className="h1">Vim Landing Page</h1>
+			<h1 className="h1-prefix">Vim Landing Page</h1>
 			<h2 className="h2">Vim Landing Page</h2>
+			<h2 className="h2-prefix">Vim Landing Page</h2>
 			<p className="paragraph">
 				Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet.
 				Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.
