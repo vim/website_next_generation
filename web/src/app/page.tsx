12b7d7866e153642cf456712f06a8371c474b4f0
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-23 11:32:03 +0100

    wip(web): add some example tags to observe behaviour of styling

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -9,9 +9,24 @@
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Vim Landing Page</h1>
-			<button className="btn inverted">Download</button>
+		<main>
+			<h1 className="h1">Vim Landing Page</h1>
+			<h2 className="h2">Vim Landing Page</h2>
+			<p className="paragraph">
+				Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet.
+				Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.
+				Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+				officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur
+				duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis. Lorem ipsum dolor sit amet, officia
+				excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+				Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit
+				irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing
+				id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint
+				cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+			</p>
+			<a className="link" href="#">
+				asdf
+			</a>
 		</main>
 	);
 }
