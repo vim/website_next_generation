3813fa60d0aa24c378f3cec90e3132ae79e9dc8e
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-23 17:12:00 +0100

    refactor(web): replace achor tag with next/link, replace arbitrary tailwind classes

--- web/src/components/Layout/Footer/Footer.tsx
+++ web/src/components/Layout/Footer/Footer.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
 	return (
 		<footer className="w-full flex justify-center items-center">
-			<a className="link" href="/impressum">
+			<Link className="link" href="/impressum">
 				Impressum
-			</a>
-			<span className="border-x-2 border-primary h-16 mx-[48px] w-0" />
-			<a className="link" href="/copyright">
+			</Link>
+			<span className="border-x-2 border-primary h-16 mx-12" />
+			<Link className="link" href="/copyright">
 				Copyright
-			</a>
+			</Link>
 		</footer>
 	);
 }
