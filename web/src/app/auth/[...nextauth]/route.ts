5cb5c20a0379063addf8d931ec036f1aa95faf9c
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-23 19:29:22 +0100

    feat(web): add dynamic route handler for nextAuth, add github as provider

---
+++ web/src/app/auth/[...nextauth]/route.ts
@@ -0,0 +1,15 @@
+import GithubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth/next";
+
+export const authOptions = {
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID ?? "",
+			clientSecret: process.env.GITHUB_SECRET ?? "",
+		}),
+	],
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
