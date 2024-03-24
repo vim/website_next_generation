ab5225b7d50562db04e4251d7e0b7c1c2b5c6b57
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-24 10:35:12 +0100

    feat(web): add basic setup for credentials provider

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth/next";
 
 const authOptions = {
@@ -6,6 +7,24 @@ const authOptions = {
 		GithubProvider({
 			clientId: process.env.GITHUB_ID ?? "",
 			clientSecret: process.env.GITHUB_SECRET ?? "",
+		}),
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				username: { label: "Username", type: "text", placeholder: "jsmith" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials) {
+				const res = await fetch("/your/endpoint", {
+					method: "POST",
+					body: JSON.stringify(credentials),
+					headers: { "Content-Type": "application/json" },
+				});
+				const user = await res.json();
+
+				if (res.ok && user) return user;
+				else return null;
+			},
 		}),
 	],
 };
