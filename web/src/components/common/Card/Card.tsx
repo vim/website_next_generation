55f17864d34fe0bf18a086c19d8fe2265c8a3274
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-28 20:03:00 +0100

    refactor(web): card now uses new types and renders content correctly

--- web/src/components/common/Card/Card.tsx
+++ web/src/components/common/Card/Card.tsx
@@ -1,10 +1,16 @@
 "use client";
 import React from "react";
+import { SingleElementSection } from "@/types/section";
 
 type CardProps = {
-	data: unknown;
+	content: SingleElementSection;
 };
 
-export default function Card({}: CardProps) {
-	return <div className="border-primary border-2 p-5 rounded-2xl">Card Content</div>;
+export default function Card({ content }: CardProps) {
+	return (
+		<div className="border-primary border-2 p-5 rounded-2xl">
+			<h2 className="h2-prefix">{content.headline}</h2>
+			{content.description && <p className="paragraph">{content.description}</p>}
+		</div>
+	);
 }
