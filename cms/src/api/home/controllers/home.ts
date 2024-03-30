679eaed888a3aa27b9e37797ea89f27330367a7a
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-30 19:23:08 +0100

    feat(cms): add homepage as single type

---
+++ cms/src/api/home/controllers/home.ts
@@ -0,0 +1,14 @@
+/**
+ * home controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController("api::home.home", ({ strapi }) => ({
+	async find(ctx) {
+		const response = await super.find(ctx);
+		const news = await strapi.entityService.findMany("api::newspost.newspost");
+
+		return { ...response, news };
+	},
+}));
