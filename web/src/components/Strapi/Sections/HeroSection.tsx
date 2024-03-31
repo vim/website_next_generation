1eea5f1aeb13ed269ed0aa5682b2a04938fab733
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-31 14:47:02 +0200

    feat(web): add all missing props to HeroSection according to strapi definitions

--- web/src/components/Strapi/Sections/HeroSection.tsx
+++ web/src/components/Strapi/Sections/HeroSection.tsx
@@ -1,19 +1,28 @@
+import Link from "next/link";
 import { CTA } from "@/types/strapi";
+import List from "../Common/List";
+
+type ListItem = {
+	id: number;
+	item: string;
+};
 
 export type HeroSectionProps = {
-	headline: string;
-	list: string[];
-	cta: CTA;
+	headline?: string;
+	cta?: CTA;
+	listItems?: ListItem[];
 };
-export default function HeroSection({ headline, list, cta }: HeroSectionProps) {
+
+export default function HeroSection({ headline, cta, listItems }: HeroSectionProps) {
 	return (
-		<div>
-			<h1>{headline}</h1>
-			<ul>
-				{list.map(el => {
-					return <li>{el}</li>;
-				})}
-			</ul>
+		<div className="mb-8">
+			{headline && <h1 className="h1-prefix">{headline}</h1>}
+			{cta && (
+				<Link className="btn-primary mb-4" href={cta.url}>
+					{cta.text}
+				</Link>
+			)}
+			{listItems && <List items={listItems} />}
 		</div>
 	);
 }
