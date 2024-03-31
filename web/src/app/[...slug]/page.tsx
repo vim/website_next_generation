8661b95683295579aa3be42b7a208fff800e24fd
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-03-31 14:52:14 +0200

    refactor(web): remove dynamic content rendering due to new type structure

--- web/src/app/[...slug]/page.tsx
+++ web/src/app/[...slug]/page.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { getPageContent } from "@/helpers/getPageContent";
-import { ContentEntry } from "@/components/common/ContentEntry/ContentEntry";
 
 export default async function page({ params }: { params: { slug: string[] } }) {
 	const pageContent = await getPageContent(params.slug);
 	return (
 		<div>
 			<h1 className="h1-prefix">{pageContent.attributes.pageTitle}</h1>
-			{pageContent.attributes.contentSection.map(contentEntry => {
-				return <ContentEntry key={contentEntry.id} entry={contentEntry} />;
-			})}
 		</div>
 	);
 }
