330f515d60bc4de60a8335af76512c0630ab5674
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-04-01 18:26:18 +0200

    wip(web): add method for rendering correct component based on response of strapi

---
+++ web/src/helpers/renderMatchingComponent.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+import NewsSection from "@/components/Strapi/Sections/NewsSection";
+import { ContentEntry } from "@/types/strapi";
+
+export function renderMatchingComponent(entry: ContentEntry): ReactNode {
+	return null;
+}
