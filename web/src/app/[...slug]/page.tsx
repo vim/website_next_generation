a03c3940ae98ef389751db6ecdba1698af3d5d36
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-04-01 18:27:39 +0200

    wip(web): add dummy code to ensure correct compiling

--- web/src/app/[...slug]/page.tsx
+++ web/src/app/[...slug]/page.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { getPageContent } from "@/helpers/getPageContent";
 
 export default async function page({ params }: { params: { slug: string[] } }) {
-	const pageContent = await getPageContent(params.slug);
+	//const pageContent = await getPageContent(params.slug);
 	return (
 		<div>
-			<h1 className="h1-prefix">{pageContent.attributes.pageTitle}</h1>
+			<h1 className="h1-prefix">Placeholder</h1>
 		</div>
 	);
 }
