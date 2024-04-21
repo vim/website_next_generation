2292b614dbd1918e53f45b3eb43ac83b61b2f7cc
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-04-21 14:01:05 +0200

    fix(web): fix lint errror on page

--- web/src/helpers/getPageContent.ts
+++ web/src/helpers/getPageContent.ts
@@ -1,8 +1,6 @@
-import { notFound } from "next/navigation";
 import qs from "qs";
-import { PageContent } from "@/types/page";
 
-export async function getPageContent(slug: string[]): Promise<PageContent> {
+export async function getPageContent(slug: string[]) {
 	const params = {
 		nested: true,
 		populate: {
@@ -14,19 +12,19 @@ export async function getPageContent(slug: string[]): Promise<PageContent> {
 		},
 	};
 	const query = qs.stringify(params, { addQueryPrefix: true });
-	const response = await fetch(`${process.env.CMS_API}/menus/1${query}`, {
+	const respose = await fetch(`${process.env.CMS_API}/menus/1${query}`, {
 		// headers: {
 		//     authorization: `Bearer ${process.env.CMS_TOKEN}`,
 		// },
 	});
-
-	const data = (await response.json()).data;
+	const data = (await respose.json()).data;
 	const page = data.attributes.items.data.find((item: { attributes: { url: string } }) => item.attributes.url.endsWith(slug[0]));
 	const pageRelation = page?.attributes?.page_relation.data;
 
-	if (!pageRelation) notFound();
-
-	const pageRes = await fetch(`${process.env.CMS_API}/pages/${pageRelation.id}?populate=*`);
-	const pageContent = (await pageRes.json()).data;
-	return pageContent;
+	let pageContent;
+	if (pageRelation) {
+		const pageRes = await fetch(`${process.env.CMS_API}/pages/${pageRelation.id}?populate=*`);
+		pageContent = (await pageRes.json()).data;
+	}
+	return { page: pageContent };
 }
