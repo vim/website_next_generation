ec27590195911e3d85f42f3befe9137a9b14bb20
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-04-21 13:38:49 +0200

    wip(web): fix linting for page

--- web/src/app/[...slug]/page.tsx
+++ web/src/app/[...slug]/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default async function page({ params }: { params: { slug: string[] } }) {
-	console.log(params);
 	return (
 		<div>
 			<h1 className="h1-prefix">Placeholder</h1>
+			<p>{params.slug[0]}</p>
 		</div>
 	);
 }
