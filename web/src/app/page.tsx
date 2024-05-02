7f51c7ebab883321d9a0adad3e7bea303622a17a
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-05-02 18:53:50 +0200

    refactor(web): remove unused code and dependencies

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
 import useSWR from "swr";
 import Hero from "@/components/Strapi/Sections/HeroSection";
 import PageContent from "@/components/Strapi/Sections/Content";
@@ -8,7 +7,6 @@ import { PageAttributes } from "@/types/strapi";
 const fetcher = (url: string) => fetch(url).then(res => res.json() as Promise<PageAttributes>);
 
 export default function Home() {
-	// const [pageData, setPageData] = useState<PageAttributes | undefined>(undefined);
 	const { data } = useSWR("/api/homepage", fetcher);
 
 	return (
