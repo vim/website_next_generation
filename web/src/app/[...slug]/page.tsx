f39156bc2d10aa9b2df23afc2f7ed1fc8c6084b9
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-05-09 14:24:54 +0200

    refactor(web): readd render logic of slug page

--- web/src/app/[...slug]/page.tsx
+++ web/src/app/[...slug]/page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { getPageContent } from "@/helpers/getPageContent";
 
 export default async function page({ params }: { params: { slug: string[] } }) {
-	return (
-		<div>
-			<h1 className="h1-prefix">Placeholder</h1>
-			<p>{params.slug[0]}</p>
-		</div>
-	);
+	try {
+		const { page } = await getPageContent(params.slug);
+		return (
+			<div>
+				<h1>{page.pageTitle}</h1>
+			</div>
+		);
+	} catch (e) {
+		return (
+			<div>
+				<p>No content defined in CMS.</p>
+			</div>
+		);
+	}
 }
