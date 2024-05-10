bbc6bcc221e4ae69950e5919266941558562d738
hanEck <hannes.eckelt@t-online.de>
2024-05-10 09:27:16 +0200

    fix(cms): Fix linting issues, define strapi type

--- cms/src/seed.ts
+++ cms/src/seed.ts
@@ -1,6 +1,5 @@
-export async function createUserSeed(strapi, amountOfUsers: number){
+export async function createUserSeed(strapi: { entityService: { create: (arg0: string, arg1: { data: { username: string; email: string; provider: string; password: string; resetPasswordToken: any; confirmationToken: any; confirmed: boolean; blocked: boolean; role: number; old_pw_hash: any; migrated_pw: boolean; }; }) => any; }; }, amountOfUsers: number){
     console.log("Function called")
-
     for (let i = 0; i < amountOfUsers; i++) {
         const user = {
             username: `dummyUser${i}`,
