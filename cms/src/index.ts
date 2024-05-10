faed47ead144c9ef9ae41fbdb49361c63539d846
hanEck <hannes.eckelt@t-online.de>
2024-05-10 09:02:43 +0200

    fix(cms): Fix linting issues

--- cms/src/index.ts
+++ cms/src/index.ts
@@ -16,33 +16,20 @@ export default {
 	 * This gives you an opportunity to set up your data model,
 	 * run jobs, or perform some special logic.
 	 */
-	 async bootstrap ({ strapi }){
-		const user = {
-			"username": "dummyUser",
-			"email": "dummyUser@example.com",
-			"provider": "local",
-			"password": "dummyPassword",
-			"resetPasswordToken": null,
-			"confirmationToken": null,
-			"confirmed": true,
-			"blocked": false,
-			"role": 1,
-			"old_pw_hash": null,
-			"migrated_pw": false
-		  }
-		  try{
+	async bootstrap ({ strapi }) {
+		try{
 			const count = await strapi.entityService.count("plugin::users-permissions.user")
-			if(count ===0){
-				createUserSeed(strapi, 10)
-				console.log("SUCCESS: Created user on bootstrap")
+			if(count === 0){
+				createUserSeed(strapi, 10);
+				console.log("SUCCESS: Created user on bootstrap");
 			}
 			else{
-				console.log("INFO: User have already been created on bootstrap")
+				console.log("INFO: User have already been created on bootstrap");
 			}
-		  }
-		  catch(err){
+		}
+		catch(err){
 			console.log("ERROR: Could not create user on bootstrap")
-			console.log(err.details)
-		  }
-     }
+			console.log(err.details);
+		}
+	}
 };
