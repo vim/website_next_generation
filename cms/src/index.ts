fb6f357fa0bc92bbf8a05ace6eb610cc775d0a06
hanEck <hannes.eckelt@t-online.de>
2024-05-10 08:35:02 +0200

    feat(cms/index.ts): Add seeding of user on bootstrap

--- cms/src/index.ts
+++ cms/src/index.ts
@@ -14,5 +14,34 @@ export default {
 	 * This gives you an opportunity to set up your data model,
 	 * run jobs, or perform some special logic.
 	 */
-	bootstrap(/*{ strapi }*/) {},
+	 async bootstrap ({ strapi }){
+		// First, find the role by its name
+		const user = {
+			"username": "dummyUser",
+			"email": "dummyUser@example.com",
+			"provider": "local",
+			"password": "dummyPassword",
+			"resetPasswordToken": null,
+			"confirmationToken": null,
+			"confirmed": true,
+			"blocked": false,
+			"role": 1,
+			"old_pw_hash": null,
+			"migrated_pw": false
+		  }
+		  try{
+			const count = await strapi.entityService.count("plugin::users-permissions.user")
+			if(count ===0){
+				await strapi.entityService.create("plugin::users-permissions.user", {data: user})
+				console.log("SUCCESS: Created user on bootstrap")
+			}
+			else{
+				console.log("INFO: User have already been created on bootstrap")
+			}
+		  }
+		  catch(err){
+			console.log("ERROR: Could not create user on bootstrap")
+			console.log(err.details)
+		  }
+     }
 };
