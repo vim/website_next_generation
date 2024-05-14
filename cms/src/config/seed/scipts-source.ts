543b08e59584b1852af64494934a57f28f10a12c
hanEck <hannes.eckelt@t-online.de>
2024-05-14 10:27:23 +0200

    feat(cms): Add multiple script sources for each script in seeding

--- cms/src/config/seed/scipts-source.ts
+++ cms/src/config/seed/scipts-source.ts
@@ -2,25 +2,30 @@ import { Strapi } from "./interfaces";
 
 export async function seedScirptsSource(strapi: Strapi) {
 	try {
-		const count = await strapi.entityService.count("api::script-source.script-source");
-		if (count === 0) {
-			const scriptSource = {
-				script_source_id: 1,
-				script: 1,
-				mime_type: "text/x-python",
-				vim_version: "8.2",
-				script_version: "1.0",
-				version_comment: "Initial version",
-				created_by_user: 789,
-				creation_date: "2024-04-20T10:00:00Z",
-				package: "sample-package",
-			};
-			await strapi.entityService.create("api::script-source.script-source", { data: scriptSource });
+		const countOfScriptSource = await strapi.entityService.count("api::script-source.script-source");
+		const countOfScript = await strapi.entityService.count("api::script.script");
+		const amountOfSourcePerScript = 3;
+		if (countOfScriptSource === 0) {
+			for (let i_script = 0; i_script < countOfScript; i_script++) {
+				for (let i_script_source = 0; i_script_source < amountOfSourcePerScript; i_script_source++) {
+					const scriptSource = {
+						script: i_script + 1,
+						mime_type: "text/x-python",
+						vim_version: "8.2",
+						script_version: "1.0",
+						version_comment: "Initial version",
+						created_by_user: 789,
+						creation_date: "2024-04-20T10:00:00Z",
+						package: "sample-package",
+					};
+					await strapi.entityService.create("api::script-source.script-source", { data: scriptSource });
+				}
+			}
 		} else {
-			console.log("INFO: Scdetailsript source have already been created on bootstrap");
+			console.log("INFO: Script source have already been created on bootstrap");
 		}
 	} catch (error) {
-		console.log("ERROR: Could not create scripts source on bootstrap");
+		console.log("ERROR: Could not create script source on bootstrap");
 		console.log(error);
 	}
 }
--- cms/src/config/seed/scripts.ts
+++ cms/src/config/seed/scripts.ts
@@ -3,22 +3,25 @@ import { Strapi } from "./interfaces";
 export async function seedScripts(strapi: Strapi) {
 	try {
 		const count = await strapi.entityService.count("api::script.script");
+		const amountOfScripts = 5;
 		if (count === 0) {
-			const script = {
-				id: 1,
-				script_name: "SampleScript",
-				script_type: "Automation",
-				summary: "A sample script for automating routine tasks.",
-				description: "This script automates various routine tasks such as file backup and system maintenance.",
-				install_details: "Download the script file and execute it on your system.",
-				ratings: 4,
-				rating_score: 80,
-				last_updated_by: 123,
-				last_update_date: "2024-05-12T15:30:00Z",
-				created_by_user: 456,
-				creation_date: "2024-04-20T10:00:00Z",
-			};
-			await strapi.entityService.create("api::script.script", { data: script });
+			for (let i = 0; i < amountOfScripts; i++) {
+				const script = {
+					id: i + 1,
+					script_name: "SampleScript",
+					script_type: "Automation",
+					summary: "A sample script for automating routine tasks.",
+					description: "This script automates various routine tasks such as file backup and system maintenance.",
+					install_details: "Download the script file and execute it on your system.",
+					ratings: 4,
+					rating_score: 80,
+					last_updated_by: 123,
+					last_update_date: "2024-05-12T15:30:00Z",
+					created_by_user: 456,
+					creation_date: "2024-04-20T10:00:00Z",
+				};
+				await strapi.entityService.create("api::script.script", { data: script });
+			}
 			console.log("SUCCESS: Created scripts on bootstrap");
 		} else {
 			console.log("INFO: Scripts have already been created on bootstrap");
