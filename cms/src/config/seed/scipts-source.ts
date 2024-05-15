a8d78e5390eba93af87aabbfb6fe39d6961636bc
hanEck <hannes.eckelt@t-online.de>
2024-05-15 08:50:38 +0200

    feat(cms): Add relation of script source to user

--- cms/src/config/seed/scipts-source.ts
+++ cms/src/config/seed/scipts-source.ts
@@ -14,9 +14,9 @@ export async function seedScirptsSource(strapi: Strapi) {
 						vim_version: "8.2",
 						script_version: "1.0",
 						version_comment: "Initial version",
-						created_by_user: 789,
 						creation_date: "2024-04-20T10:00:00Z",
 						package: "sample-package",
+						user: 1,
 					};
 					await strapi.entityService.create("api::script-source.script-source", { data: scriptSource });
 				}
