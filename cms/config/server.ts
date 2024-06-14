a8ca1250e7d42aff1dadb37c356806d79759b2c5
Johannes <johannes.munker@gmail.com>
2024-06-14 09:49:57 +0200

    feat(cms): add cms prefix to url on non local envs

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
 	host: env("HOST", "0.0.0.0"),
 	port: env.int("PORT", 1337),
+	url: env.bool("NODE_ENV", "local") === "local" ? "" : "/cms",
 	app: {
 		keys: env.array("APP_KEYS"),
 	},
