40db849d3b20a298c00cac38c495a6842bbe7462
Fabian Untermoser <fabian@ederbit.xyz>
2024-06-14 17:57:09 +0900

    feat(cms): add URL variable

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
 	host: env("HOST", "0.0.0.0"),
 	port: env.int("PORT", 1337),
-	url: env.bool("NODE_ENV", "local") === "local" ? "" : "/cms",
+	url: env("URL", ""),
 	app: {
 		keys: env.array("APP_KEYS"),
 	},
