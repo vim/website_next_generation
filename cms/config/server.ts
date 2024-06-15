54b932c7d1d7078e49960ae2d7ff170d3edbe1ef
Johannes <johannes.munker@gmail.com>
2024-06-15 10:45:53 +0200

    feat(cms): hardcode url

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
 	host: env("HOST", "0.0.0.0"),
 	port: env.int("PORT", 1337),
-	url: env("URL", ""),
+	url: env("NODE_ENV") === "local" ? "" : "https://preview.vim.org/cms",
 	app: {
 		keys: env.array("APP_KEYS"),
 	},
