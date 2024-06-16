dbccfe497548aba55fad77c67478b41241cbe496
Fabian Untermoser <fabian@ederbit.xyz>
2024-06-16 17:17:38 +0900

    feat(cms): set CMS_URL at buildtime
    - add Dockerfile arg
    - set arg in github action through secret

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
 	host: env("HOST", "0.0.0.0"),
 	port: env.int("PORT", 1337),
-	url: env("NODE_ENV") === "development" ? "" : "https://preview.vim.org/cms",
+	url: env("NODE_ENV") === "development" ? "" : env("CMS_URL"),
 	app: {
 		keys: env.array("APP_KEYS"),
 	},
