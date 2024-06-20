09dcf0ce55e6407f9e8e4a4c0704066b2e59338a
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-06-20 13:20:54 +0200

    feat(web): add custom 500 error page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,11 +3,17 @@ import useSWR from "swr";
 import Hero from "@/components/Strapi/Sections/HeroSection";
 import PageContent from "@/components/Strapi/Sections/Content";
 import { PageAttributes, Routes } from "@/types/strapi";
+import Error from "next/error";
+import { redirect } from "next/navigation";
 
 const fetcher = (url: string) => fetch(url).then(res => res.json() as Promise<PageAttributes>);
 
 export default function Home() {
-	const { data } = useSWR(`/api/${Routes.homepage}`, fetcher);
+	const { data, error } = useSWR<PageAttributes, Error>(`/api/${Routes.homepage}`, fetcher);
+
+	if (error) {
+		redirect("/error/500");
+	}
 
 	return (
 		<main className="pb-12">
