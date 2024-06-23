9571bbe0a5cb2984580720f9466b204dab8ee09e
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-06-23 09:28:56 +0200

    feat(web): add error handling to data fetch on homepage

--- web/src/app/api/[homepage]/route.ts
+++ web/src/app/api/[homepage]/route.ts
@@ -1,5 +1,6 @@
 import { getNews } from "@/helpers/homepage";
 import { SingleType } from "@/types/strapi";
+import { notFound, redirect } from "next/navigation";
 
 export async function GET() {
 	const res = await fetch(`${process.env.CMS_API}/home?populate=deep`, {
@@ -9,18 +10,28 @@ export async function GET() {
 	});
 
 	if (!res.ok) {
+		handleHTTPError(res.status);
 		throw new Error("Fetching single type component home failed");
 	}
 
-	const homePageData = (await res.json()) as SingleType;
+	let homePageData: SingleType;
 
-	if (!homePageData.data.attributes.body) {
+	try {
+		homePageData = (await res.json()) as SingleType;
+	} catch (e) {
+		console.log(e);
+		throw new Error("Failed to parse homepage data");
+	}
+
+	if (!homePageData?.data.attributes.body) {
 		return Response.json("Homepage contains no data");
 	}
 
 	const newsSections = homePageData.data.attributes.body.flatMap((contentEntry, i) => {
-		if ("newsCount" in contentEntry) return { index: i, newsCount: contentEntry.newsCount, headline: contentEntry.headline };
-		else return [];
+		if ("newsCount" in contentEntry) {
+			return { index: i, newsCount: contentEntry.newsCount, headline: contentEntry.headline };
+		}
+		return [];
 	});
 
 	for (const newsSectionEntry of newsSections) {
@@ -30,4 +41,15 @@ export async function GET() {
 	}
 
 	return Response.json(homePageData.data.attributes);
+}
+
+function handleHTTPError(errorCode: number) {
+	switch (errorCode) {
+		case 400:
+			return notFound();
+		case 500:
+			return redirect("/error/500");
+		default:
+			throw new Error("Fetching homepage data failed with status: " + errorCode);
+	}
 }
